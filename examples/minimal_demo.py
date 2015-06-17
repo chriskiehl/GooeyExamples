25b969bd8ca8f8ec0dd2094a7b44e942c9453e1b
chriskiehl <audionautic@gmail.com>
2015-06-16 23:07:22 -0400

    initial commit after fork from main gooey project

---
+++ examples/minimal_demo.py
@@ -0,0 +1,23 @@
+'''
+A minimal Gooey example. One required field, one optional. 
+'''
+
+
+import argparse
+from gooey import Gooey
+
+@Gooey(dump_build_config=True)
+def main():
+  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser.add_argument('integers', metavar='N', nargs='+',
+                     help='an integer for the accumulator')
+  parser.add_argument('--sum', dest='accumulate', action='store_const',
+                     const=sum, default=max,
+                     help='sum the integers (default: find the max)')
+
+  args = parser.parse_args()
+  print args.accumulate(args.integers)
+
+
+if __name__ == '__main__':
+  main()
