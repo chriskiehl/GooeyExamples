89479875025c064e5b4e9a01376999d43ea40cf2
eladeyal-intel <elad.eyal@intel.com>
2020-02-21 13:55:13 +0200

    remove incosisent dashes
    Prior to this commit, each arguments except the first
    was prepended with a dash, which was confusing.

--- examples/message.py
+++ examples/message.py
@@ -26,7 +26,7 @@ See ya!
 
 
 def display_message():
-    message = program_message.format('\n-'.join(sys.argv[1:])).split('\n')
+    message = program_message.format('\n'.join(sys.argv[1:])).split('\n')
     delay = 1.8 / len(message)
 
     for line in message:
