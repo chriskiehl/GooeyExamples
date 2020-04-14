e3cf5dd41f4ff081b5d8d2a8bf2f95d31abad67a
Bess L. Walker <blwalker@google.com>
2020-04-13 20:32:10 -0600

    Update example MultiFileChooser argument definition to include nargs.

--- examples/widget_demo.py
+++ examples/widget_demo.py
@@ -43,7 +43,7 @@ def main():
     my_cool_parser.add_argument(
         "FileSaver", help=file_help_msg, widget="FileSaver")
     my_cool_parser.add_argument(
-        "MultiFileSaver", help=file_help_msg, widget="MultiFileChooser")
+        "MultiFileSaver", nargs='*', help=file_help_msg, widget="MultiFileChooser")
     my_cool_parser.add_argument("directory", help="Directory to store output")
 
     my_cool_parser.add_argument('-d', '--duration', default=2,
