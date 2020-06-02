25d370d880e729c89e0c18c4b1b2c1a2a889f865
Ben Elliston <bje@air.net.au>
2020-06-02 22:03:07 +1000

    Fix flake8 warnings.

--- examples/richtext_demo.py
+++ examples/richtext_demo.py
@@ -5,11 +5,12 @@ A Gooey example showing use of richtext display in the console pannel.
 from gooey import Gooey, GooeyParser
 from colored import stylize, attr, fg
 
+
 @Gooey(richtext_controls=True, auto_start=True)
 def main():
     parser = GooeyParser(description='Just display the console')
 
-    args = parser.parse_args()
+    parser.parse_args()
     print(stylize("This is bold.", attr("bold")))
     print(stylize("This is underlined.", attr("underlined")))
     print(stylize("This is green.", fg("green")))
