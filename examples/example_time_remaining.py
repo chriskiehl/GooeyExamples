a7333d6c09d3838dcf8b1a0430b1742358ecf399
Jack McKew <jackmckew2@gmail.com>
2020-11-06 13:56:07 +1100

    Update as per closed PR

--- examples/example_time_remaining.py
+++ examples/example_time_remaining.py
@@ -3,11 +3,14 @@ import os
 
 @Gooey(optional_cols=2,
         program_name="Elapsed / Remaining Timer on Progress in Gooey",
+
         progress_regex=r"^progress: (?P<current>\d+)/(?P<total>\d+)$",
         progress_expr="current / total * 100",
-        timing_options = {
-                'show_time_remaining':True,
-                'show_time_remaining_on_complete':True
+        hide_progress_msg=True,
+
+        timing_options={
+            'show_time_remaining':True,
+            'hide_time_remaining_on_complete':True
         }
         )
 def parse_args():
@@ -18,13 +21,17 @@ def parse_args():
 
     range_parser = sub_parsers.add_parser('range')
 
-    range_parser.add_argument('--length',default=10000)
+    range_parser.add_argument('--length',default=10)
 
     return parser.parse_args()
 
 def compute_range(length):
 
     for i in range(length):
+        # print("sometin")
+        import time
+        from random import randint
+        time.sleep(randint(1,3))
         print(f"progress: {i}/{length}")
 
 if __name__ == '__main__':
